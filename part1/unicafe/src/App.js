9725edebd63613ab7a062bb04a700f0ca7b6f534
Abdirahman Suldaan <suldaan.89@hotmail.com>
2021-12-07 19:23:21 +0200

    1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,7 +9,8 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  // const eventHandler = (props) => pr
+  let all = good + neutral + bad
+  // const eventHandler = (props) => 
 
   return (
     <div>
@@ -21,6 +22,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Average {(good-bad)/all}</p>
+      <p>Positive {good / all * 100} %</p>
     </div>
   )
 }
