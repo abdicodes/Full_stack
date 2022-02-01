97583d848a57b25fc1e1c558e592301263c6999e
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-02-02 01:44:41 +0200

    show button fixed, cleaner code

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+/*this function will an array of countries as argument and compare each element of the array to a given string.
+if the string is in the country name it will push the element eventally return an array of all the partial or full matches 
+in case the string is empty it will return an empty array */
 const search = (array, char) => {
   if (char) {
     const a = [];
@@ -11,8 +14,11 @@ const search = (array, char) => {
     });
     return a;
   }
+  // returns an empty array if the above condition is not met
   return [];
 };
+
+/* this component returns a single country  */
 const Details = ({ country }) => {
   return (
     <div>
@@ -21,12 +27,11 @@ const Details = ({ country }) => {
       <h4>Population: {country.population}</h4>
       <h2>Languages</h2>
       <ul>
-        {country.languages.forEach((e, i) => {
-          <li id={i}> {e.name}</li>;
-        })}
+        {country.languages.map((element, i) => (
+          <li key={i}> {element.name}</li>
+        ))}
       </ul>
       <div>
-        {" "}
         <img src={country.flags.png} alt="flag" />
       </div>
     </div>
@@ -36,6 +41,7 @@ const App = () => {
   const [countries, setCountries] = useState([]);
   const [countrySearch, setCountrySearch] = useState("");
   const [searchList, setSearchList] = useState([]);
+  const [countryDetails, setCountryDetails] = useState(null);
 
   useEffect(() => {
     console.log("effect");
@@ -50,15 +56,35 @@ const App = () => {
   const eventHandler = (e) => {
     setCountrySearch(e.target.value);
   };
+  useEffect(() => {
+    setCountryDetails(null);
+  }, [countrySearch]);
 
   const CountryList = ({ countries }) => {
-    return (
+    return countryDetails === null ? (
       <div>
-        {countries.length < 10 ? (
-          countries.map((e, i) => <div key={i}>{e.name}</div>)
+        {countries.length <= 10 ? (
+          countries.map((e, i) => (
+            <div key={i}>
+              {e.name}{" "}
+              <button
+                type="submit"
+                onClick={(event) => {
+                  event.preventDefault();
+                  setCountryDetails(e);
+                }}
+              >
+                show
+              </button>
+            </div>
+          ))
         ) : (
           <p>Too many matches, Specify another filter</p>
         )}
+      </div>
+    ) : (
+      <div>
+        <Details country={countryDetails} />{" "}
       </div>
     );
   };
@@ -69,12 +95,6 @@ const App = () => {
           find countries <input onChange={eventHandler} />
         </div>
       </form>
-      {/* <div>
-        {countries.map((e, i) => (
-          <div key={i}>{e.name}</div>
-        ))}
-      </div> */}
-      {/* <CountryList countries={searchList} /> */}
 
       <div>
         {searchList.length === 1 ? (
