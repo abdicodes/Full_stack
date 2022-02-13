3f18fbfe62fd45db34a02ff42b2ff2c546b5b82d
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-02-13 09:50:23 +0200

    exercies 3.1-3.6

--- part3/index.js
+++ part3/index.js
@@ -1,23 +1,26 @@
+const e = require("express");
 const express = require("express");
 const app = express();
-let notes = [
+let persons = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
 ];
 
@@ -27,9 +30,8 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
-  response.json(notes);
-  console.log(notes);
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
 });
 app.get("/api/notes/:id", (request, response) => {
   const id = request.params.id;
@@ -40,6 +42,54 @@ app.get("/api/notes/:id", (request, response) => {
     response.json(note);
   } else {
     response.status(404).end();
+  }
+});
+app.get("/info", (req, res) => {
+  const date = new Date();
+  const entries = persons.length;
+  res.send(`<h3>Phonebook has info for ${entries} people <br> ${date}</h3>`);
+});
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const person = persons.find((person) => person.id === id);
+  console.log(person);
+  if (person) {
+    res.json(person);
+    console.log("OK");
+  } else {
+    res.status(404).end();
+    console.log("not OK");
+  }
+});
+app.delete("/api/persons/:id", (req, res) => {
+  id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+app.post("/api/persons", (req, res) => {
+  const id = Math.round(Math.random() * 1000000);
+  contact = req.body;
+  if (persons.find((person) => person.name === contact.name)) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
+  }
+  console.log(contact);
+  if (contact.name && contact.number) {
+    const person = {
+      name: contact.name,
+      number: contact.number,
+      id: id,
+    };
+    console.log(person);
+    persons = persons.concat(person);
+    console.log(persons);
+    res.json(person);
+  } else {
+    return res.status(400).json({
+      error: "The name or number is missing",
+    });
   }
 });
 
