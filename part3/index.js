2b2eafa10aa8ab33471ee4540185b36480f9b118
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-04-07 15:30:02 +0300

    setting MongoDB env

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
+const Note = require("./models/note");
+
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
@@ -68,7 +71,9 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 app.get("/api/notes/:id", (request, response) => {
   const id = request.params.id;
@@ -187,7 +192,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
