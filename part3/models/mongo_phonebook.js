8cfe121ddeae7410f5780ddfd27d241c6f285468
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-04-10 15:43:16 +0300

    last commit part 3 ESLint inegrated

---
+++ part3/models/mongo_phonebook.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log(
+    'Please provide the password as an argument: node mongo.js <password>'
+  )
+  process.exit(1)
+}
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://abdicodes:${password}@cluster0.5yjqd.mongodb.net/phoneBook?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: Number,
+})
+
+const Person = mongoose.model('Person', personSchema)
+if (!name && !number) {
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  const person = new Person({
+    name: name,
+    number: number,
+    id: Person.length + 1,
+  })
+
+  person.save().then((result) => {
+    console.log(`added ${result.name} number ${result.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
--- part3/models/persons.js
+++ part3/models/persons.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI_PERSONS;
+const url = process.env.MONGODB_URI_PERSONS
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 3 },
@@ -21,20 +21,20 @@ const personSchema = new mongoose.Schema({
     minlength: 8,
     validate: {
       validator: function (v) {
-        return /\d{2,3}\-{0,1}\d{7,9}/.test(v);
+        return /\d{2,3}-{0,1}\d{7,9}/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
   },
   id: String,
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
