3863a6ff9fb4780867d868eae755acbe5e6f0444
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-04-10 16:29:00 +0300

    fix: minor fix for updating contact and ESLint

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,84 +1,87 @@
-import React, { useState, useEffect } from "react";
-import Filter from "./Filter";
-import PersonForm from "./PersonForm";
-import Persons from "./Persons";
-import backEndLogic from "./backEndLogic";
-import Notification from "./Notification";
+import React, { useState, useEffect } from 'react'
+import Filter from './Filter'
+import PersonForm from './PersonForm'
+import Persons from './Persons'
+import backEndLogic from './backEndLogic'
+import Notification from './Notification'
 //this functions iterates through the App state "persons" and checks if the name matches with string "name"
 const isContact = (array, name) => {
-  return array.find((e) => e.name === name);
-};
+  return array.find((e) => e.name === name)
+}
 
 // this function will take an array and string characters as inputs. string is coming from user input against elements
 const Searching = (char, array) => {
   if (char) {
-    const a = [];
+    const a = []
     array.forEach((element) => {
       if (element.name.toLowerCase().includes(char.toLowerCase())) {
-        a.push(element);
+        a.push(element)
       }
-    });
-    return a;
+    })
+    return a
   }
-  return array;
-};
+  return array
+}
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setnewNumber] = useState("");
-  const [filtered, setFiltered] = useState(persons);
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [confirmMessage, setConfirmMessage] = useState(null);
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setnewNumber] = useState('')
+  const [filtered, setFiltered] = useState(persons)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [confirmMessage, setConfirmMessage] = useState(null)
   useEffect(() => {
-    setFiltered(persons);
-  }, [persons]);
+    setFiltered(persons)
+  }, [persons])
   useEffect(() => {
-    backEndLogic.getAll().then((res) => setPersons(res));
-  }, []);
+    backEndLogic.getAll().then((res) => setPersons(res))
+  }, [])
 
   const filterHandler = (event) => {
-    const char = event.target.value;
-    setFiltered(Searching(char, persons));
-  };
+    const char = event.target.value
+    setFiltered(Searching(char, persons))
+  }
 
   const addPerson = (event) => {
     //adds a person if the string is not empty and uses isContact function to check if name in newName exist
-    event.preventDefault();
+    event.preventDefault()
 
     //logic to check if the name already exists and use PUT to modify.
-    if (newName === "") return;
-    const existing_contact = isContact(persons, newName);
-    if (existing_contact) {
+    if (newName === '') return
+    const existingContact = isContact(persons, newName)
+    if (existingContact) {
       if (
         window.confirm(
           `${newName} already added to phonebook. Replace the old number with a new one?`
         )
       ) {
         backEndLogic
-          .modify(existing_contact.id, {
-            ...existing_contact,
+          .modify(existingContact.id, {
+            ...existingContact,
             number: newNumber,
           })
-          .then((res) => {
+          .then((updatedContact) => {
             setPersons(
-              persons.filter((person) => person.id !== res.id).concat(res)
-            );
-            setNewName("");
-            setnewNumber("");
-            setConfirmMessage(`Contact has succesfully been updated`);
+              persons.map((person) =>
+                person.id === updatedContact.id ? updatedContact : person
+              )
+            )
+            console.log(persons)
+            setNewName('')
+            setnewNumber('')
+            setConfirmMessage(`Contact has succesfully been updated`)
             setTimeout(() => {
-              setConfirmMessage(null);
-            }, 4000);
+              setConfirmMessage(null)
+            }, 4000)
           })
           .catch((error) => {
-            setErrorMessage(error.response.data.error);
-            setNewName("");
-            setnewNumber("");
+            setErrorMessage(error.response.data.error)
+            setNewName('')
+            setnewNumber('')
             setTimeout(() => {
-              setErrorMessage(null);
-            }, 4000);
-          });
+              setErrorMessage(null)
+            }, 4000)
+          })
       }
       // logic to add a new contact using POST
     } else {
@@ -88,60 +91,60 @@ const App = () => {
           number: newNumber,
         })
         .then((res) => {
-          setPersons(persons.concat(res));
-          setNewName("");
-          setnewNumber("");
-          setConfirmMessage(`Contact has succesfully been added`);
+          setPersons(persons.concat(res))
+          setNewName('')
+          setnewNumber('')
+          setConfirmMessage(`Contact has succesfully been added`)
           setTimeout(() => {
-            setConfirmMessage(null);
-          }, 4000);
+            setConfirmMessage(null)
+          }, 4000)
         })
         .catch((error) => {
-          setErrorMessage(error.response.data.error);
-          setNewName("");
-          setnewNumber("");
+          setErrorMessage(error.response.data.error)
+          setNewName('')
+          setnewNumber('')
           setTimeout(() => {
-            setErrorMessage(null);
-          }, 4000);
-        });
+            setErrorMessage(null)
+          }, 4000)
+        })
     }
-  };
+  }
   const nameInputHandler = (e) => {
-    setNewName(e.target.value);
-  };
+    setNewName(e.target.value)
+  }
   const numberInputHandler = (e) => {
-    setnewNumber(e.target.value);
-  };
+    setnewNumber(e.target.value)
+  }
   const errorMessageHandler = (e) => {
     try {
-      backEndLogic(e);
+      backEndLogic(e)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
   const deleteHandler = (id, name) => {
     if (!window.confirm(`Are you sure you want to delete ${name} ?`)) {
-      return;
+      return
     }
     backEndLogic
       .deleteContact(id)
       .then((res) => {
-        setPersons(persons.filter((person) => person.id !== res.id));
-        setConfirmMessage(`${name} has succesfully been removed`);
+        setPersons(persons.filter((person) => person.id !== res.id))
+        setConfirmMessage(`${name} has succesfully been removed`)
         setTimeout(() => {
-          setConfirmMessage(null);
-        }, 4000);
+          setConfirmMessage(null)
+        }, 4000)
       })
       .catch((e) => {
-        console.log(e);
+        console.log(e)
         setErrorMessage(
           `information on ${name} has already been removed from server`
-        );
+        )
         setTimeout(() => {
-          setErrorMessage(null);
-        }, 4000);
-      });
-  };
+          setErrorMessage(null)
+        }, 4000)
+      })
+  }
 
   return (
     <div>
@@ -164,7 +167,7 @@ const App = () => {
       <h2>Numbers</h2>
       <Persons contacts={filtered} deleteHandler={deleteHandler} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- part2/phonebook/src/backEndLogic.js
+++ part2/phonebook/src/backEndLogic.js
@@ -1,23 +1,24 @@
-import axios from "axios";
-const baseURL = "/api/persons";
+import axios from 'axios'
+const baseURL = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
-  const request = axios.get(baseURL);
-  return request.then((response) => response.data);
-};
+  const request = axios.get(baseURL)
+  return request.then((response) => response.data)
+}
 const create = (contact) => {
-  const request = axios.post(baseURL, contact);
-  return request.then((response) => response.data);
-};
+  const request = axios.post(baseURL, contact)
+  return request.then((response) => response.data)
+}
 
 const modify = (id, contact) => {
-  const request = axios.put(`${baseURL}/${id}`, contact);
-  return request.then((response) => response.data);
-};
+  const request = axios.put(`${baseURL}/${id}`, contact)
+  return request.then((response) => response.data)
+}
 
 const deleteContact = (id) => {
-  const request = axios.delete(`${baseURL}/${id}`);
-  return request.then((res) => res.data);
-};
+  const request = axios.delete(`${baseURL}/${id}`)
+  return request.then((res) => res.data)
+}
 
-export default { getAll, create, modify, deleteContact, baseURL };
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getAll, create, modify, deleteContact, baseURL }
