2e937a0506c2984bbc1805b4a8178a1be67fddde
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-04-10 20:28:27 +0300

    init new router for blogs and inhanced it

--- part4/index.js
+++ part4/index.js
@@ -1,26 +1,15 @@
 const config = require('./utils/config')
 const express = require('express')
+const blogsRouter = require('./controllers/blogsRouter')
 const app = express()
+
 const cors = require('cors')
-const Blog = require('./models/blogs')
+
 const logger = require('./utils/logger')
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (req, res) => {
-  Blog.find({}).then((blogs) => {
-    res.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog.save().then((result) => {
-    res.status(201).json(result)
-  })
-})
+app.use(blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
