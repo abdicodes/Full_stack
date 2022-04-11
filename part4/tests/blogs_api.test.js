f5bad6715e9d82da938de99fbba0437217c3e71f
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-04-12 02:05:38 +0300

    unit testing HTTP request & id property of DB

--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -6,11 +6,20 @@ const api = supertest(app)
 
 test('notes are returned as json', async () => {
   await api
-    .get('/api/notes')
+    .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
+test('there are two blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(2)
+})
+test('id property is defined', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
 afterAll(() => {
   mongoose.connection.close()
 })
