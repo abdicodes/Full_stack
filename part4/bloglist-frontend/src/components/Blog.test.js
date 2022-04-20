d369baf2ea214edf66753a600f34b9267d40c246
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-04-20 14:21:21 +0300

    blog list unit test 2

--- part4/bloglist-frontend/src/components/Blog.test.js
+++ part4/bloglist-frontend/src/components/Blog.test.js
@@ -4,46 +4,33 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    title: 'Component testing is done with react-testing-library',
-    author: 'me',
-    likes: 5,
-  }
-
-  render(<Blog blog={blog} />)
-
-  const element = screen.getByText(
-    'Component testing is done with react-testing-library'
-  )
-
-  expect(element).toBeDefined()
-})
-
-test('renders content', () => {
-  const blog = {
-    title: 'Component testing is done with react-testing-library',
-    author: 'me',
-    likes: 5,
-  }
-
-  const { container } = render(<Blog blog={blog} />)
-
-  const div = container.querySelector('.blog')
-  expect(div).toHaveTextContent(
-    'Component testing is done with react-testing-library'
-  )
-})
+const blog = {
+  title: 'the best blog',
+  author: 'Future blogger',
+  likes: 5,
+  id: '625780e6bc56c91c48f2c7f5',
+  user: '625780e6bc56c91c48f2c7f4',
+}
+const user = {
+  name: 'Abdi',
+  username: 'test',
+  id: '625780e6bc56c91c48f2c7f4',
+}
+beforeEach(() => {})
 test('expect author and likes to not be rendered', () => {
-  const blog = {
-    title: 'the best blog',
-    author: 'Future blogger',
-    likes: 5,
-  }
   render(<Blog blog={blog} />)
   const title = screen.queryByText('Future blogger')
-  const likes = screen.queryByText(5)
+  const likes = screen.queryByText('5')
   expect(title).toBeNull()
   expect(likes).toBeNull()
-  screen.debug()
+})
+test('check if author and likes are rendered after view button click', async () => {
+  const element = render(<Blog blog={blog} user={user} />)
+  element.debug()
+  const button = screen.getByText('view')
+  userEvent.click(button)
+  const title = screen.queryByText('Future blogger')
+  const likes = screen.queryByText('5')
+  expect(title).toBeDefined()
+  expect(likes).toBeDefined()
 })
