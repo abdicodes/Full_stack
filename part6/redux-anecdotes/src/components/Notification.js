b68ee64a54cc3aaddc92746a2b83577f43ab404b
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-04-24 18:52:00 +0300

    redux connect in Notification component

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,13 +1,19 @@
-import { useSelector } from 'react-redux'
+// import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
+const Notification = ({ notifications }) => {
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
   }
-  const notification = useSelector((state) => state.notifications)
-  return notification ? <div style={style}>{notification}</div> : null
-}
 
-export default Notification
+  // const notification = useSelector((state) => state.notifications)
+  return notifications ? <div style={style}>{notifications}</div> : null
+}
+const mapStatetoProps = (state) => {
+  return { notifications: state.notifications }
+}
+// export default Notification
+
+export default connect(mapStatetoProps)(Notification)
