032df75d79388737b5482e4c2f08860fbec35ad8
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-05-06 12:19:57 +0300

    graphQL query books by author optionally

--- part8/index.js
+++ part8/index.js
@@ -92,7 +92,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
   type Book {
@@ -114,7 +114,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) return books
+
+      return books.filter((book) => book.author === args.author)
+    },
     allAuthors: () => {
       return authors.map(
         (author) =>
