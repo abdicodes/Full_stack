30b975a070b00add9793bc596719e1fe641f9fb7
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-05-19 18:45:17 +0300

    9.6-9.7 POST request setup with error handling

--- part9/9b/exerciseCalculator.ts
+++ part9/9b/exerciseCalculator.ts
@@ -1,71 +1,72 @@
-type rating = 1 | 2 | 3;
+type rating = 1 | 2 | 3
 
 interface ExerciseResult {
-    periodLength: number;
-    trainingDays: number;
-    success: boolean;
-    rating: rating;
-    ratingDescription: string;
-    target: number;
-    average: number;
+  periodLength: number
+  trainingDays: number
+  success: boolean
+  rating: rating
+  ratingDescription: string
+  target: number
+  average: number
 }
 
 interface Inputs {
-    array: Array<number>;
-    rating: number;
+  array: Array<number>
+  target: number
 }
 const parseArguments2 = (args: Array<string>): Inputs => {
-    if (args.length < 4) throw new Error('Not enough arguments');
-    if (args.length > 15) throw new Error('Too many arguments');
-    let a = [];
-    for (let i = 3; i < args.length; i++) {
-        if (isNaN(Number(args[i]))) {
-            throw new Error('Provided values were not numbers!');
-        }
-        a.push(Number(args[i]));
+  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length > 15) throw new Error('Too many arguments')
+  let a = []
+  for (let i = 3; i < args.length; i++) {
+    if (isNaN(Number(args[i]))) {
+      throw new Error('Provided values were not numbers!')
     }
+    a.push(Number(args[i]))
+  }
 
-    return {
-        array: a,
-        rating: Number(args[2]),
-    };
-};
+  return {
+    array: a,
+    target: Number(args[2]),
+  }
+}
 
-const calculateExercises = (
-    arg1: Array<number>,
-    arg2: number
+export const calculateExercises = (
+  arg1: Array<number>,
+  arg2: number
 ): ExerciseResult => {
-    const trainingDays = arg1.filter((day) => day !== 0).length;
-    const average = arg1.reduce((sum, day) => sum + day, 0) / arg1.length;
-    const success = average > arg2;
-    const rating: rating =
-        average < 1.75 ? 1 : average > 1.75 && average < 2.25 ? 2 : 3;
-    const target = arg2;
-    const ratingDescription =
-        average < 1.75
-            ? 'underperformed'
-            : average > 1.75 && average < 2.25
-            ? 'average performance'
-            : ' great performance';
+  const trainingDays = arg1.filter((day) => day !== 0).length
+  const average = arg1.reduce((sum, day) => sum + day, 0) / arg1.length
+  const success = average > arg2
+  const rating: rating =
+    average < 1.75 ? 1 : average > 1.75 && average < 2.25 ? 2 : 3
+  const target = arg2
+  const ratingDescription =
+    average < 1.75
+      ? 'underperformed'
+      : average > 1.75 && average < 2.25
+      ? 'average performance'
+      : ' great performance'
 
-    return {
-        periodLength: arg1.length,
-        trainingDays: trainingDays,
-        success: success,
-        rating: rating,
-        target: target,
-        ratingDescription: ratingDescription,
-        average: average,
-    };
-};
+  return {
+    periodLength: arg1.length,
+    trainingDays: trainingDays,
+    success: success,
+    rating: rating,
+    target: target,
+    ratingDescription: ratingDescription,
+    average: average,
+  }
+}
 
 try {
-    const { array, rating } = parseArguments2(process.argv);
-    console.log(calculateExercises(array, rating));
+  const { array, target } = parseArguments2(process.argv)
+  console.log(calculateExercises(array, target))
 } catch (error: unknown) {
-    let errorMessage = 'Something bad happened.';
-    if (error instanceof Error) {
-        errorMessage += ' Error: ' + error.message;
-    }
-    console.log(errorMessage);
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log(errorMessage)
 }
+// module.exports = calculateExercises;
--- part9/9b/index.ts
+++ part9/9b/index.ts
@@ -1,9 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import express = require('express');
-
+import { calculateExercises } from './exerciseCalculator';
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
     res.send('full stack development');
+});
+app.post('/exercises', (_req, res) => {
+    console.log(_req.body);
+    if (!_req.body.array || !_req.body.target) {
+        return res.send({ error: 'parameters missing' }).status(400);
+    }
+    console.log('it has passed this line ');
+    const { array, target } = _req.body;
+    if (
+        isNaN(
+            array.reduce(
+                (sum: number, day: number | string) => sum + Number(day),
+                0
+            )
+        ) ||
+        isNaN(Number(target))
+    ) {
+        return res.send({ error: 'malformatted parameters' }).status(400);
+    }
+    return res.send(calculateExercises(array, target));
 });
 
 const PORT = 3003;
