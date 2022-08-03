daa4ce797b53d34e8319f0c0798a5df4b691d548
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-08-03 18:01:43 +0300

    refactored Part component with appropriate types

--- part9/9d/src/Part.tsx
+++ part9/9d/src/Part.tsx
@@ -1,8 +1,47 @@
-import { PartsComponent } from './types'
+// import { PartsComponent } from './types'
+import { CoursePart } from './types'
+/**
+ * Helper function for exhaustive type checking
+ */
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  )
+}
 
-const Part = (props: PartsComponent) => (
-  <p>
-    {props.name} {props.exerciseCount}
-  </p>
-)
+const Part = ({ part }: { part: CoursePart }) => {
+  switch (part.type) {
+    case 'normal':
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br /> {part.description}
+        </p>
+      )
+    case 'groupProject':
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br /> project exercises {part.groupProjectCount}
+        </p>
+      )
+    case 'submission':
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br /> {part.description}
+          <br /> submit to {part.exerciseSubmissionLink}
+        </p>
+      )
+    default:
+      return assertNever(part)
+  }
+}
+
 export default Part
