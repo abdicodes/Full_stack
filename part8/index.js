8e542f2fac17e52acf1bcaf83ba6ef71007ce3b9
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-01-17 06:43:58 +0200

    refactor

--- part8/index.js
+++ part8/index.js
@@ -1,51 +1,38 @@
-require('dotenv').config()
-const jwt = require('jsonwebtoken')
-const { execute, subscribe } = require('graphql')
-const { SubscriptionServer } = require('subscriptions-transport-ws')
-const { ApolloServer } = require('apollo-server-express')
-const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core')
-const { makeExecutableSchema } = require('@graphql-tools/schema')
-const express = require('express')
-const http = require('http')
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { execute, subscribe } = require('graphql');
+const { SubscriptionServer } = require('subscriptions-transport-ws');
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const express = require('express');
+const http = require('http');
 
-const mongoose = require('mongoose')
-const { v1: uuid } = require('uuid')
-const User = require('./models/users')
-const typeDefs = require('./graphql/typedefs')
-const resolvers = require('./graphql/resolver')
+const mongoose = require('mongoose');
+const { v1: uuid } = require('uuid');
+const User = require('./models/users');
+const typeDefs = require('./graphql/typedefs');
+const resolvers = require('./graphql/resolver');
 
-const JWT_SECRET = 'NEED_HERE_A_SECRET_KEY'
+const JWT_SECRET = 'NEED_HERE_A_SECRET_KEY';
 
-const db = process.env.MONGODB_URI_LIBRARY
-console.log('connecting to', db)
+const db = process.env.MONGODB_URI_LIBRARY;
+console.log('connecting to', db);
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('connected to MongoDB')
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
-  })
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: async ({ req }) => {
-//     const auth = req ? req.headers.authorization : null
-//     if (auth && auth.toLowerCase().startsWith('bearer ')) {
-//       const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
-//       const currentUser = await User.findById(decodedToken.id)
-//       return { currentUser }
-//     }
-//   },
-// })
+    console.log('error connection to MongoDB:', error.message);
+  });
 
 const start = async () => {
-  const app = express()
-  const httpServer = http.createServer(app)
+  const app = express();
+  const httpServer = http.createServer(app);
 
-  const schema = makeExecutableSchema({ typeDefs, resolvers })
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
   const subscriptionServer = SubscriptionServer.create(
     {
       schema,
@@ -56,15 +43,15 @@ const start = async () => {
       server: httpServer,
       path: '',
     }
-  )
+  );
   const server = new ApolloServer({
     schema,
     context: async ({ req }) => {
-      const auth = req ? req.headers.authorization : null
+      const auth = req ? req.headers.authorization : null;
       if (auth && auth.toLowerCase().startsWith('bearer ')) {
-        const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
-        const currentUser = await User.findById(decodedToken.id)
-        return { currentUser }
+        const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET);
+        const currentUser = await User.findById(decodedToken.id);
+        return { currentUser };
       }
     },
     plugins: [
@@ -73,28 +60,28 @@ const start = async () => {
         async serverWillStart() {
           return {
             async drainServer() {
-              subscriptionServer.close()
+              subscriptionServer.close();
             },
-          }
+          };
         },
       },
     ],
-  })
+  });
 
-  await server.start()
+  await server.start();
 
   server.applyMiddleware({
     app,
     path: '/',
-  })
+  });
 
-  const PORT = 4000
+  const PORT = 4000;
 
   httpServer.listen(PORT, () =>
     console.log(`Server is now running on http://localhost:${PORT}`)
-  )
-}
-start()
+  );
+};
+start();
 
 // server.listen().then(({ url }) => {
 //   console.log(`Server ready at ${url}`)
